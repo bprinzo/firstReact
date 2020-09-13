d8d58621eb83ed3852ac93cb284f4b627fc8ddd0
Bruno Tufanin Prinzo <brunoprinzo@hotmail.com>
2020-09-13 03:04:49 -0300

    Updates

---
+++ precache-manifest.01c083bcadc85df3cc31a083dee84e5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd52b58a45f8014b41d97744b8bf8c5f",
+    "url": "/firstReact/index.html"
+  },
+  {
+    "revision": "e0292cc9215346812e1a",
+    "url": "/firstReact/static/css/main.cfc24bf0.chunk.css"
+  },
+  {
+    "revision": "6a1ee60f49eda731d7b5",
+    "url": "/firstReact/static/js/2.49381729.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/firstReact/static/js/2.49381729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0292cc9215346812e1a",
+    "url": "/firstReact/static/js/main.7ef5742c.chunk.js"
+  },
+  {
+    "revision": "7ce3c0e3c6ecde09c9b2",
+    "url": "/firstReact/static/js/runtime-main.6f222131.js"
+  },
+  {
+    "revision": "8f57009f69134405d6a0bcd4344592d3",
+    "url": "/firstReact/static/media/logo.8f57009f.png"
+  }
+]);
